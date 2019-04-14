baa2f59e9b2526fdd6e89a28cd06fcf064a43300
christian <christiancazu@gmail.com>
2019-04-13 23:47:59 -0500

    practice2

--- app/src/main/java/com/utp/app/MainActivity.java
+++ app/src/main/java/com/utp/app/MainActivity.java
@@ -2,12 +2,22 @@ package com.utp.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // declaring variables
+    private TextView txv;
+    private boolean switcher = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting txv with the id assigned in activity_main.xml
+        txv = findViewById(R.id.txvMessage);
     }
 }

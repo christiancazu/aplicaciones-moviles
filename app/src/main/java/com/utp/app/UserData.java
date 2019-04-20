7985ada3b8c521b9363a166ab4e9b1be7606eafd
unknown <Administrador@UTP208728.grupoutp.net>
2019-04-20 13:14:43 -0500

    advance 2

--- app/src/main/java/com/utp/app/UserData.java
+++ app/src/main/java/com/utp/app/UserData.java
@@ -2,6 +2,8 @@ package com.utp.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class UserData extends AppCompatActivity {
 
@@ -9,6 +11,16 @@ public class UserData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_data);
+
+        User user = (User) getIntent().getParcelableExtra("data");
+
+        TextView txtName = findViewById(R.id.txt_name);
+        TextView txtPwd = findViewById(R.id.txt_pwd);
+
+        txtName.setText(user.getName());
+        txtPwd.setText(user.getPassword());
+
+        Toast.makeText(this, user.getName(), Toast.LENGTH_SHORT).show();
     }
 
 

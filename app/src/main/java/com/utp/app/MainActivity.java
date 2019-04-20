b0f317070f9fc902bb20f9c717ea404f99d855e6
unknown <Administrador@UTP208728.grupoutp.net>
2019-04-20 12:37:30 -0500

    advance

--- app/src/main/java/com/utp/app/MainActivity.java
+++ app/src/main/java/com/utp/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.utp.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Authenticate(View v) {
+        EditText txtName = findViewById(R.id.txt_name);
+        EditText txtPwd = findViewById(R.id.txt_pwd);
+
+        User user = new User(
+                txtName.getText().toString(),
+                txtPwd.getText().toString());
+
+        Intent intent = new Intent(this, UserData.class);
+        intent.putExtra("data", user);
     }
 }

0eaea15d479f022c9a0b87b7c83f575441058dcc
christian <christiancazu@gmail.com>
2019-05-09 16:55:06 -0500

    working with mediaplayer

--- app/src/main/java/com/utp/app/MainActivity.java
+++ app/src/main/java/com/utp/app/MainActivity.java
@@ -1,13 +1,27 @@
 package com.utp.app;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+    Button btnPlay;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnPlay = findViewById(R.id.btn_play);
+        mp = MediaPlayer.create(this, R.raw.ringtone);
     }
+
+    public void playSong(View v) {
+        mp.start();
+    }
+
 }

9643b8c90eb8261f8adc60227c048f9919f378c2
christian <christiancazu@gmail.com>
2019-05-11 08:21:16 -0500

    implemented playpause btn functionality

--- app/src/main/java/com/utp/app/MusicPlayer.java
+++ app/src/main/java/com/utp/app/MusicPlayer.java
@@ -1,7 +1,9 @@
 package com.utp.app;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ public class MusicPlayer extends AppCompatActivity {
 
     Song song;
     ImageButton btnPlayPause;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,29 @@ public class MusicPlayer extends AppCompatActivity {
         initBtnPlay();
 
         initSelectedSong();
+
+        initResSongWithSelectedSong();
+
+        // #TODO:
+        //      timer count regressive
+        //      functionality for previous/next buttons
+        //      gif animation for content_music_player image background
+    }
+
+    public void onPlayPause(View v) {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+            btnPlayPause.setImageResource(R.drawable.ic_play);
+        } else {
+            mediaPlayer.start();
+            btnPlayPause.setImageResource(R.drawable.ic_pause);
+        }
+    }
+
+    protected void initResSongWithSelectedSong() {
+        int id = getResources().getIdentifier(song.getName(), "raw", this.getPackageName());
+
+        mediaPlayer = MediaPlayer.create(this, id);
     }
 
     protected void initSelectedSong() {

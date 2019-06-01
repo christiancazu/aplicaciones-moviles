c2d49b79daff25147d7f89b4f9fb1946542da0e6
Christian <christiancazu@gmail.com>
2019-06-01 02:16:17 -0500

    refactored to use only one particle

--- app/src/main/java/com/utp/app/MainActivity.java
+++ app/src/main/java/com/utp/app/MainActivity.java
@@ -31,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
     private Display mDisplay;
     private PowerManager.WakeLock mWakeLock;
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -107,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
         private float mHorizontalBound;
         private float mVerticalBound;
         private final ParticleSystem mParticleSystem;
+
         /*
          * Each of our particle holds its previous and current position, its
          * acceleration. for added realism each particle has its own friction
@@ -138,8 +141,8 @@ public class MainActivity extends AppCompatActivity {
 
             public void computePhysics(float sx, float sy, float dT) {
 
-                final float ax = -sx/5;
-                final float ay = -sy/5;
+                final float ax = -sx / 5;
+                final float ay = -sy / 5;
 
                 mPosX += mVelX * dT + ax * dT * dT / 2;
                 mPosY += mVelY * dT + ay * dT * dT / 2;
@@ -180,19 +183,16 @@ public class MainActivity extends AppCompatActivity {
          * A particle system is just a collection of particles
          */
         class ParticleSystem {
-            static final int NUM_PARTICLES = 5;
-            private Particle mBalls[] = new Particle[NUM_PARTICLES];
+            private Particle mBall;
 
             ParticleSystem() {
                 /*
                  * Initially our particles have no speed or acceleration
                  */
-                for (int i = 0; i < mBalls.length; i++) {
-                    mBalls[i] = new Particle(getContext());
-                    mBalls[i].setBackgroundResource(R.drawable.ball);
-                    mBalls[i].setLayerType(LAYER_TYPE_HARDWARE, null);
-                    addView(mBalls[i], new ViewGroup.LayoutParams(mDstWidth, mDstHeight));
-                }
+                mBall = new Particle(getContext());
+                mBall.setBackgroundResource(R.drawable.ball);
+                mBall.setLayerType(LAYER_TYPE_HARDWARE, null);
+                addView(mBall, new ViewGroup.LayoutParams(mDstWidth, mDstHeight));
             }
 
             /*
@@ -203,11 +203,8 @@ public class MainActivity extends AppCompatActivity {
                 final long t = timestamp;
                 if (mLastT != 0) {
                     final float dT = (float) (t - mLastT) / 1000.f /** (1.0f / 1000000000.0f)*/;
-                    final int count = mBalls.length;
-                    for (int i = 0; i < count; i++) {
-                        Particle ball = mBalls[i];
-                        ball.computePhysics(sx, sy, dT);
-                    }
+                    Particle ball = mBall;
+                    ball.computePhysics(sx, sy, dT);
                 }
                 mLastT = t;
             }
@@ -231,52 +228,49 @@ public class MainActivity extends AppCompatActivity {
                  * stiffness.
                  */
                 boolean more = true;
-                final int count = mBalls.length;
                 for (int k = 0; k < NUM_MAX_ITERATIONS && more; k++) {
                     more = false;
-                    for (int i = 0; i < count; i++) {
-                        Particle curr = mBalls[i];
-                        for (int j = i + 1; j < count; j++) {
-                            Particle ball = mBalls[j];
-                            float dx = ball.mPosX - curr.mPosX;
-                            float dy = ball.mPosY - curr.mPosY;
-                            float dd = dx * dx + dy * dy;
-                            // Check for collisions
-                            if (dd <= sBallDiameter2) {
-                                /*
-                                 * add a little bit of entropy, after nothing is
-                                 * perfect in the universe.
-                                 */
-                                dx += ((float) Math.random() - 0.5f) * 0.0001f;
-                                dy += ((float) Math.random() - 0.5f) * 0.0001f;
-                                dd = dx * dx + dy * dy;
-                                // simulate the spring
-                                final float d = (float) Math.sqrt(dd);
-                                final float c = (0.5f * (sBallDiameter - d)) / d;
-                                final float effectX = dx * c;
-                                final float effectY = dy * c;
-                                curr.mPosX -= effectX;
-                                curr.mPosY -= effectY;
-                                ball.mPosX += effectX;
-                                ball.mPosY += effectY;
-                                more = true;
-                            }
-                        }
-                        curr.resolveCollisionWithBounds();
+                    Particle curr = mBall;
+                    Particle ball = mBall;
+                    float dx = ball.mPosX - curr.mPosX;
+                    float dy = ball.mPosY - curr.mPosY;
+                    float dd = dx * dx + dy * dy;
+                    // Check for collisions
+                    if (dd <= sBallDiameter2) {
+                        /*
+                         * add a little bit of entropy, after nothing is
+                         * perfect in the universe.
+                         */
+                        dx += ((float) Math.random() - 0.5f) * 0.0001f;
+                        dy += ((float) Math.random() - 0.5f) * 0.0001f;
+                        dd = dx * dx + dy * dy;
+                        // simulate the spring
+                        final float d = (float) Math.sqrt(dd);
+                        final float c = (0.5f * (sBallDiameter - d)) / d;
+                        final float effectX = dx * c;
+                        final float effectY = dy * c;
+                        curr.mPosX -= effectX;
+                        curr.mPosY -= effectY;
+                        ball.mPosX += effectX;
+                        ball.mPosY += effectY;
+                        more = true;
                     }
+
+                    curr.resolveCollisionWithBounds();
+
                 }
             }
 
-            public int getParticleCount() {
-                return mBalls.length;
-            }
+            /*public int getParticleCount() {
+                return 1;
+            }*/
 
             public float getPosX(int i) {
-                return mBalls[i].mPosX;
+                return mBall.mPosX;
             }
 
             public float getPosY(int i) {
-                return mBalls[i].mPosY;
+                return mBall.mPosY;
             }
         }
 
@@ -376,25 +370,21 @@ public class MainActivity extends AppCompatActivity {
             final float yc = mYOrigin;
             final float xs = mMetersToPixelsX;
             final float ys = mMetersToPixelsY;
-            final int count = particleSystem.getParticleCount();
-            for (int i = 0; i < count; i++) {
-                /*
-                 * We transform the canvas so that the coordinate system matches
-                 * the sensors coordinate system with the origin in the center
-                 * of the screen and the unit is the meter.
-                 */
-                final float x = xc + particleSystem.getPosX(i) * xs;
-                final float y = yc - particleSystem.getPosY(i) * ys;
-                particleSystem.mBalls[i].setTranslationX(x);
-                particleSystem.mBalls[i].setTranslationY(y);
-            }
+            /*
+             * We transform the canvas so that the coordinate system matches
+             * the sensors coordinate system with the origin in the center
+             * of the screen and the unit is the meter.
+             */
+            final float x = xc + particleSystem.getPosX(0) * xs;
+            final float y = yc - particleSystem.getPosY(0) * ys;
+            particleSystem.mBall.setTranslationX(x);
+            particleSystem.mBall.setTranslationY(y);
 
             // and make sure to redraw asap
             invalidate();
         }
 
         @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
     }
 }

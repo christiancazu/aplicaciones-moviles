f3b7e6823f09f6921d881751503eb6f769094704
unknown <Administrador@UTP208728.grupoutp.net>
2019-06-22 12:20:52 -0500

    implemented: internal storage

---
+++ app/src/main/java/com/utp/app/InternalStorage.java
@@ -0,0 +1,50 @@
+package com.utp.app;
+
+import android.content.Context;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InternalStorage {
+
+    private static final String FILENAME = "myInternalStorage";
+
+    public static void setData(Context context, String input) throws IOException {
+        FileOutputStream fos = null;
+        try {
+            fos = context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
+            fos.write(input.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getData(Context context) throws IOException {
+        FileInputStream fis = null;
+        fis = context.openFileInput(FILENAME);
+        String data = getFileContent(fis, "UTF-8");
+        fis.close();
+
+        return data;
+    }
+
+    private static String getFileContent(FileInputStream fis, String encoding) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(fis, encoding)))
+        {
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append((line));
+                sb.append('\n');
+            }
+            return sb.toString();
+        }
+    }
+
+}
--- app/src/main/java/com/utp/app/MainActivity.java
+++ app/src/main/java/com/utp/app/MainActivity.java
@@ -6,26 +6,42 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText etValue;
-    private TextView tvSharedPreferences;
+    private TextView tvSharedPreferences, tvInternalStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etValue = findViewById(R.id.et_value);
-        tvSharedPreferences = findViewById(R.id.tv_shared_preferences);
+        initWidgetsBinding();
     }
 
-    public void sharedPreferences_OnClickSave(View v) {
+    public void sharedPreferencesOnClickSave(View v) {
         MySharedPreferences.setData(this, etValue.getText().toString());
     }
 
-    public void sharedPreferences_OnClickRead(View v) {
+    public void sharedPreferencesOnClickRead(View v) {
         tvSharedPreferences.setText(MySharedPreferences.getData(this));
+    }
+
+    public void internalStorageOnClickSave(View v) throws IOException {
+        InternalStorage.setData(this, etValue.getText().toString());
+    }
+
+    public void internalStorageOnClickRead(View v) throws IOException {
+        tvInternalStorage.setText(InternalStorage.getData(this));
+    }
+
+    private void initWidgetsBinding() {
+        etValue = findViewById(R.id.et_value);
+
+        tvSharedPreferences = findViewById(R.id.tv_shared_preferences);
+        tvInternalStorage = findViewById(R.id.tv_internal_storage);
     }
 
 }

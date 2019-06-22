616774243e8f6df3442327f22192bea901a34786
Christian <christiancazu@gmail.com>
2019-06-22 05:04:25 -0500

    implemented: MySharedPreferences

---
+++ app/src/main/java/com/utp/app/MySharedPreferences.java
@@ -0,0 +1,24 @@
+package com.utp.app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MySharedPreferences {
+
+    private static String PREF_NAME = "prefs";
+
+    private static SharedPreferences getPrefs(Context context) {
+        return context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+    }
+
+    public static String getToken(Context context) {
+        return getPrefs(context).getString("token", "");
+    }
+
+    public static void setToken(Context context, String input) {
+        SharedPreferences.Editor editor = getPrefs(context).edit();
+        editor.putString("token", input);
+        editor.commit();
+    }
+
+}
